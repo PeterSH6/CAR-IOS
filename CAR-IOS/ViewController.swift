b2de353614b3ec6978eee1106f1b4738e35e690f
PeterSH6 <13302211862@189.cn>
2020-07-27 20:18:12 +0800

    Inital commit

---
+++ CAR-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CAR-IOS
+//
+//  Created by 生广明 on 27/7/2020.
+//  Copyright © 2020 生广明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '12.0'
+
+target 'CAR-IOS' do
+  pod 'LibTorch', '~>1.5.0'
+  # Pods for CAR-IOS
+end
